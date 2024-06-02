cdf2ed95c9122c65039d43adcb7ed2b2a291510c
Pawel Gnat <pawel_gnat@o2.pl>
2024-06-02 22:16:59 +0200

    feat: web transitions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,23 @@
+import data from '../../data.json';
+
 export const convertToSlug = (title) => {
   const lowercaseTitle = title.toLowerCase();
   const slug = lowercaseTitle.replace(/\s+/g, '-');
   return slug;
+};
+
+export const getPreviousPageName = (index) => {
+  if (index === 0) {
+    return '';
+  }
+
+  return data[index - 1].name;
+};
+
+export const getNextPageName = (index) => {
+  if (data.length - index === 1) {
+    return '';
+  }
+
+  return data[index + 1].name;
 };
